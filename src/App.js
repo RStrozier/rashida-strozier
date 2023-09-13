0e80740087e693a191bd8aa1354f4d21f20f0e4c
Rashida <rashida.strozier@gmail.com>
2023-09-13 18:52:29 -0400

    pushing the branches

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App container-fluid-bg">
+      <div className="Message container-md p-2 m-1 rounded-2">
+      <i class="fa-regular fa-hand"></i>
+      <p className="Text-Full">
+        Just like me, this website is a 
+        <p className="Work-Text">
+          Work In Progress
+        </p>
+        <p className="Signiture">
+        -Rashida S.
+        </p>
+      </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
