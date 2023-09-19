e71c5607546d36beee57f76a4d1b80ea74a57a38
Rashida <rashida.strozier@gmail.com>
2023-09-19 09:48:00 -0400

    reeedit dom render

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import './App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,5 +10,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-
